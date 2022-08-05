d0591166ab3a946c8f7a91be2d255140294ccfa7
nhuquynhnguyenle <56589737+nhuquynhnguyenle@users.noreply.github.com>
2022-08-05 14:13:41 +0700

    Add files via upload

---
+++ Student.py
@@ -0,0 +1,12 @@
+class Student:
+    #studentName = ""
+    #studentBirthday = ""
+    #studentGender = ""
+    #studentID = ""
+    
+    def __init__(self, name, birthday,id, gender, major):
+        self.studentName = name
+        self.studentBirthday = birthday
+        self.studentID = id 
+        self.studentGender = gender
+        self.studentMajor = major
